def2e7206c86e00bcf073b99ea1a6294bb7ede51
iagorrr <iagorrr04@gmail.com>
2025-01-22 16:36:26 -0300

    addd prototype code

--- src/src.ino
+++ src/src.ino
@@ -1,8 +1,10 @@
-namespace Ultrasonic {
+#define DEBUG
 
-const uint8_t PIN_TRIG = 26; // D26 / 10
-const uint8_t PIN_ECHO = 27; // D27 / 11
-// Half sound speed in cm / us
+namespace Ultrasonic {
+const uint8_t PIN_TRIG = 35;
+const uint8_t PIN_ECHO = 32;
+
+//Half sound speed in cm / us
 const double HALF_SOUND_SPEED = 0.017;
 
 inline void setup() {
@@ -12,6 +14,8 @@ inline void setup() {
 
 double duration_us, distance_cm;
 inline double get_distance() {
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(2);
   digitalWrite(PIN_TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(PIN_TRIG, LOW);
@@ -20,46 +24,106 @@ inline double get_distance() {
   return distance_cm;
 }
 
-} // namespace Ultrasonic
+}
 
 namespace Motors {
-const uint8_t PIN_IN_1 = 33; // D33 / 8
-const uint8_t PIN_IN_2 = 32; // D32 / 7
-const uint8_t PIN_IN_3 = 35; // D35 / 6
-const uint8_t PIN_IN_4 = 34; // D34 / 5
+const uint8_t PINS[2][2] = {
+  { 12, 14 }, { 27, 26 }
+};
+
+
+enum Side {
+  left,
+  right
+};
+
+enum Direction {
+  foward,
+  backward
+};
+
+void move(int32_t t, Side s, Direction d) {
+  digitalWrite(PINS[s][0], d),
+    digitalWrite(PINS[s][1], !d);
+
+  delayMicroseconds(t);
+
+  digitalWrite(PINS[s][0], LOW),
+    digitalWrite(PINS[s][1], LOW);
+}
+
+void move(int32_t t, Direction d) {
+  digitalWrite(PINS[0][0], d),
+    digitalWrite(PINS[0][1], !d);
+
+  digitalWrite(PINS[1][0], d),
+    digitalWrite(PINS[1][1], !d);
+
+  delayMicroseconds(t);
+
+  digitalWrite(PINS[0][0], LOW),
+    digitalWrite(PINS[0][1], LOW);
+
+  digitalWrite(PINS[1][0], LOW),
+    digitalWrite(PINS[1][1], LOW);
+}
 
 inline void setup() {
-  pinMode(PIN_IN_1, OUTPUT);
-  pinMode(PIN_IN_2, OUTPUT);
-  pinMode(PIN_IN_3, OUTPUT);
-  pinMode(PIN_IN_4, OUTPUT);
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      pinMode(PINS[i][j], OUTPUT);
+      digitalWrite(PINS[i][j], LOW);
+    }
+  }
+
+#ifdef DEBUG
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
+  move(2 * 1e6, Side::left, Direction::foward);
+  move(2 * 1e6, Side::left, Direction::backward);
+
+  move(2 * 1e6, Side::right, Direction::foward);
+  move(2 * 1e6, Side::right, Direction::backward);
+
+  move(2 * 1e6, Direction::foward);
+  move(2 * 1e6, Direction::backward);
+
+  digitalWrite(2, LOW);
+#endif
 }
-} // namespace Motors
+}
 
 namespace Infrared {
 
 enum Infrared_position {
   FRONT_LEFT = 0,
   FRONT_RIGHT,
-  BACK_LEFT,
   BACK_RIGHT,
+  BACK_LEFT,
+
 };
-const uint8_t PIN_OUT[]{21, 19, 18, 17};
+const uint8_t PIN_OUT[]{ 15, 4, 19, 22 };
+
 
 inline void setup() {
   pinMode(PIN_OUT[Infrared_position::FRONT_LEFT], INPUT);
-  pinMode(PIN_OUT[Infrared_position::FRONT_RIGHT], INPUT_PULLUP);
-  pinMode(PIN_OUT[Infrared_position::BACK_RIGHT], INPUT_PULLUP);
-  pinMode(PIN_OUT[Infrared_position::BACK_LEFT], INPUT_PULLUP);
+  pinMode(PIN_OUT[Infrared_position::FRONT_RIGHT], INPUT);
+  pinMode(PIN_OUT[Infrared_position::BACK_RIGHT], INPUT);
+  pinMode(PIN_OUT[Infrared_position::BACK_LEFT], INPUT);
 }
 
-enum Arena_position { OUT = 0, IN };
+enum Arena_position {
+  OUT = 0,
+  IN
+};
 
 Arena_position get_arena_position(Infrared_position p) {
   return (Arena_position)digitalRead(PIN_OUT[p]);
 }
 
-} // namespace Infrared
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -68,4 +132,19 @@ void setup() {
   Motors::setup();
 }
 
-void loop() {}
+
+
+void loop() {
+#ifdef DEBUG
+  static int idx;
+  Serial.printf("%4d \t IRFL %d \t IRFR %d \t IRBR %d \t IRBL %d \t US %0.2f\n",
+                idx++,
+                Infrared::get_arena_position(Infrared::FRONT_LEFT),
+                Infrared::get_arena_position(Infrared::FRONT_RIGHT),
+                Infrared::get_arena_position(Infrared::BACK_RIGHT),
+                Infrared::get_arena_position(Infrared::BACK_LEFT),
+                Ultrasonic::get_distance());
+  Serial.flush();
+#endif
+}
+
